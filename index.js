c1b15e6afe72a30fc56bd36279dff08a8bd2cac6
Olesya Krayanskaya <krayanskaya_ov@mail.ru>
2025-02-11 18:45:05 +0300

    отладка

--- index.js
+++ index.js
@@ -10,20 +10,35 @@ window.onload = function () {
         return;
     }
 
+    canvas.focus(); // Устанавливаем фокус на canvas
+
+    // Ждем загрузки изображений
+    mazeImg.onload = function () {
+        heroImg.onload = function () {
+            startGame(canvas, mazeImg, heroImg);
+        };
+    };
+
+    // Если изображения уже загружены, запускаем игру сразу
+    if (mazeImg.complete && heroImg.complete) {
+        startGame(canvas, mazeImg, heroImg);
+    }
+};
+
+function startGame(canvas, mazeImg, heroImg) {
     const context = canvas.getContext('2d', { willReadFrequently: true });
 
     const config = {
-        heroSize: 15,
+        heroSize: 12,
         heroColor: 'rgb(254,244,207)',
         wallColors: [
             { r: 0, g: 0, b: 0 },
-            { r: 169, g: 169, b: 169 }
         ],
-        finishLine: canvas.height - 17
+        finishLine: canvas.height
     };
 
     let x = 66;
-    let y = 0;
+    let y = 3;
     let dx = 0;
     let dy = 0;
 
@@ -46,10 +61,10 @@ window.onload = function () {
     function processKey(e) {
         e.preventDefault();
         const keyActions = {
-            37: () => (dx = -1, dy = 0),
-            38: () => (dx = 0, dy = -1),
-            39: () => (dx = 1, dy = 0),
-            40: () => (dx = 0, dy = 1)
+            37: () => (dx = -1, dy = 0), // Влево
+            38: () => (dx = 0, dy = -1), // Вверх
+            39: () => (dx = 1, dy = 0),  // Вправо
+            40: () => (dx = 0, dy = 1)   // Вниз
         };
         dx = 0;
         dy = 0;
@@ -62,6 +77,7 @@ window.onload = function () {
 
     function drawFrame() {
         if (dx !== 0 || dy !== 0) {
+            context.beginPath();
             context.fillStyle = config.heroColor;
             context.fillRect(x, y, config.heroSize, config.heroSize);
             x += dx;
@@ -88,21 +104,26 @@ window.onload = function () {
         const endY = Math.min(canvas.height, y + config.heroSize + 2);
         const width = endX - startX;
         const height = endY - startY;
+
         if (width <= 0 || height <= 0) {
             return false;
         }
+
         const imgData = context.getImageData(startX, startY, width, height);
         const pixels = imgData.data;
         for (let i = 0; i < pixels.length; i += 4) {
             const red = pixels[i];
             const green = pixels[i + 1];
             const blue = pixels[i + 2];
+
             for (const color of config.wallColors) {
                 if (red === color.r && green === color.g && blue === color.b) {
+                    console.log('столкновение')
                     return true;
                 }
             }
         }
+        console.log('едем дальше')
         return false;
     }
 };
